7501be8a844a3a9d6eef3953b50f377ea46f5164
Robert <robertong43@gmail.com>
2026-06-08 17:10:58 +0800

    Update NextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/wenrou-library',
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
