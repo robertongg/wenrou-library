ea0c466f873254bf7b12ec3588616486507a49a7
Robert <robertong43@gmail.com>
2026-06-09 13:00:38 +0800

    Update database key

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    'https://ybqbsvogavzgeubwnnkw.supabase.co',
+    'sb_publishable_9sR7pd8D1ma7KFCDO4mqJg_kf9_4LdY',
   );
 }
